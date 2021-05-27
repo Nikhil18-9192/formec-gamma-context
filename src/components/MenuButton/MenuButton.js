ab4d54ace365a8b5d72c4b5a90749c9f6233d8c7
nikhil18-9192 <nikhilkorane08@gmail.com>
2021-05-27 12:08:14 +0530

    latest

--- src/components/MenuButton/MenuButton.js
+++ src/components/MenuButton/MenuButton.js
@@ -17,13 +17,13 @@ export default function MenuButton() {
         <div className={styles.menuButton}>
             <div className={styles.burgerMenu} onClick={menuToggle}>
                 <div className={styles.burgerOpen}  >
-                    <span className={menuState ? styles.t1 : ''}  ></span>
-                    <span className={menuState ? styles.t1 : ''}></span>
-                    <span className={menuState ? styles.t1 : ''}></span>
+                    <span className={menuState ? styles.t1 : ''} style={menuState ? { transitionDelay: '0s !important' } : { transitionDelay: '0.3s' }} ></span>
+                    <span className={menuState ? styles.t1 : ''} style={menuState ? { transitionDelay: '0s !important' } : { transitionDelay: '0.3s' }}></span>
+                    <span className={menuState ? styles.t1 : ''} style={menuState ? { transitionDelay: '0s !important' } : { transitionDelay: '0.3s' }}></span>
                 </div>
                 <div className={styles.burgerClose}>
-                    <span className={!menuState ? styles.t1 : ''} ></span>
-                    <span className={!menuState ? styles.t2 : ''} ></span>
+                    <span className={!menuState ? styles.t1 : ''} style={!menuState ? { transitionDelay: '0.3s !important' } : { transitionDelay: '0s' }}></span>
+                    <span className={!menuState ? styles.t2 : ''} style={!menuState ? { transitionDelay: '0.3s !important' } : { transitionDelay: '0s' }}></span>
                 </div>
             </div>
         </div>
